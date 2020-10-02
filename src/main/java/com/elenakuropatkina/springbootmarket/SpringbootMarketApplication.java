dcb60035c1ffc05be2ddb53a999356785840554b
ElenaKuropatkina <el-vita81@yandex.ru>
2020-10-02 12:02:34 +0300

    init_commit

---
+++ src/main/java/com/elenakuropatkina/springbootmarket/SpringbootMarketApplication.java
@@ -0,0 +1,13 @@
+package com.elenakuropatkina.springbootmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMarketApplication.class, args);
+	}
+
+}
